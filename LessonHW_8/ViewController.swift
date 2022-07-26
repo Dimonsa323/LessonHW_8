675787cb8a1f47c088ffe57e744f5619981b9ff0
Dima Hubenia <dima96gubenya@gmail.com>
2022-07-26 19:02:35 +0300

    Initial Commit

---
+++ LessonHW_8/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LessonHW_8
+//
+//  Created by Дима Губеня on 26.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
